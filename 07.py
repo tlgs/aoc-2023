73a556a04c4cb0eced44905101d36fae820a9cc7
Tiago Seabra <tlgs@users.noreply.github.com>
2023-12-07 18:39:14 +0000

    Refactor day 07 using some string translations

--- 07.py
+++ 07.py
@@ -7,64 +7,41 @@ def parse_input(puzzle_input):
     return (hands,)
 
 
-def run(hands, key):
-    ranked = sorted(hands, key=key)
-    return sum(rank * int(bid) for rank, (_, bid) in enumerate(ranked, start=1))
+def hand_key(hand):
+    cards, _ = hand
+
+    filtered = cards.replace("~", "")
+    signature = tuple(sorted(Counter(filtered).values()))
+    match signature:
+        case (5,) | (4,) | (3,) | (2,) | (1,) | ():
+            kind = 6
+        case (1, 4) | (1, 3) | (1, 2) | (1, 1):
+            kind = 5
+        case (2, 3) | (2, 2):
+            kind = 4
+        case (1, 1, 3) | (1, 1, 2) | (1, 1, 1):
+            kind = 3
+        case (1, 2, 2):
+            kind = 2
+        case (1, 1, 1, 2) | (1, 1, 1, 1):
+            kind = 1
+        case (1, 1, 1, 1, 1):
+            kind = 0
+
+    strengths = tuple(map("~23456789TJQKA".index, cards))
+
+    return (kind, *strengths)
 
 
 def part_one(hands):
-    def f(hand):
-        cards, _ = hand
-
-        types = [
-            (1, 1, 1, 1, 1),
-            (1, 1, 1, 2),
-            (1, 2, 2),
-            (1, 1, 3),
-            (2, 3),
-            (1, 4),
-            (5,),
-        ]
-        signature = tuple(sorted(Counter(cards).values()))
-        kind = types.index(signature)
-
-        ordering = ["2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"]
-        strengths = tuple(map(ordering.index, cards))
-
-        return (kind, *strengths)
-
-    return run(hands, f)
+    ranked = sorted(hands, key=hand_key)
+    return sum(rank * int(bid) for rank, (_, bid) in enumerate(ranked, start=1))
 
 
 def part_two(hands):
-    def f(hand):
-        cards, _ = hand
-
-        signature = tuple(sorted(Counter(c for c in cards if c != "J").values()))
-        match signature:
-            case (5,) | (4,) | (3,) | (2,) | (1,) | ():
-                kind = 6
-            case (1, 4) | (1, 3) | (1, 2) | (1, 1):
-                kind = 5
-            case (2, 3) | (2, 2):
-                kind = 4
-            case (1, 1, 3) | (1, 1, 2) | (1, 1, 1):
-                kind = 3
-            case (1, 2, 2):
-                kind = 2
-            case (1, 1, 1, 2) | (1, 1, 1, 1):
-                kind = 1
-            case (1, 1, 1, 1, 1):
-                kind = 0
-            case t:
-                raise ValueError(t)
-
-        ordering = ["J", "2", "3", "4", "5", "6", "7", "8", "9", "T", "Q", "K", "A"]
-        strengths = tuple(map(ordering.index, cards))
-
-        return (kind, *strengths)
-
-    return run(hands, f)
+    hands = [(c.replace("J", "~"), b) for c, b in hands]
+    ranked = sorted(hands, key=hand_key)
+    return sum(rank * int(bid) for rank, (_, bid) in enumerate(ranked, start=1))
 
 
 class Test:
