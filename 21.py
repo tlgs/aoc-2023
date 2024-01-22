13acc5922244a02b232166b008955598df169264
Tiago Seabra <tlgs@users.noreply.github.com>
2024-01-22 14:39:35 +0000

    Refactor day 21
    Still shit.
    
    Benchmark 1: python 21.py <data/21.txt
      Time (mean ± σ):     26.557 s ±  3.023 s    [User: 26.342 s, System: 0.052 s]
      Range (min … max):   22.652 s … 31.839 s    10 runs
    
    Benchmark 2: pypy3 21.py <data/21.txt
      Time (mean ± σ):     11.289 s ±  1.033 s    [User: 11.129 s, System: 0.101 s]
      Range (min … max):   10.171 s … 12.880 s    10 runs
    
    Summary
      pypy3 21.py <data/21.txt ran
        2.35 ± 0.34 times faster than python 21.py <data/21.txt

--- 21.py
+++ 21.py
@@ -2,14 +2,13 @@ import sys
 
 
 def parse_input(puzzle_input):
-    grid, start = {}, None
+    grid = {}
     for y, line in enumerate(puzzle_input.splitlines()):
         for x, c in enumerate(line):
-            if c == "S":
-                start, c = (x, y), "."
-            grid[x, y] = c
+            if c in ".S":
+                grid[x, y] = c
 
-    return grid, start
+    return grid, y + 1
 
 
 def neighbors(x, y):
@@ -19,28 +18,29 @@ def neighbors(x, y):
     yield x - 1, y
 
 
-def part_one(grid, start, steps=64):
-    plots = {start}
+def part_one(grid, _, steps=64):
+    plots = {p for p in grid if grid[p] == "S"}
     for _ in range(steps):
-        plots = {n for t in plots for n in neighbors(*t) if grid.get(n) == "."}
+        plots = {n for t in plots for n in neighbors(*t) if n in grid}
 
     return len(plots)
 
 
-def part_two(grid, start, steps=26501365):
-    size = int(len(grid) ** 0.5)  # grid is a square
+def part_two(grid, size, steps=26501365):
+    plots = {p for p in grid if grid[p] == "S"}
 
     done = []
-    plots = {start}
-    for i in range(3 * size):
-        if (i % size) == (size // 2):
+    offset = size // 2
+    check = {offset, offset + size, offset + 2 * size}
+    for i in range(offset + 2 * size + 1):
+        if i in check:
             done.append(len(plots))
 
         plots = {
             (x, y)
             for t in plots
             for x, y in neighbors(*t)
-            if grid.get((x % size, y % size)) == "."
+            if (x % size, y % size) in grid
         }
 
     n = steps // size
@@ -65,15 +65,6 @@ class Test:
 
     def test_one(self):
         assert part_one(*parse_input(self.example), 6) == 16
-
-    # def test_two(self):
-    # assert part_two(*parse_input(self.example), 6) == 16
-    # assert part_two(*parse_input(self.example), 10) == 50
-    # assert part_two(*parse_input(self.example), 50) == 1594
-    # assert part_two(*parse_input(self.example), 100) == 6536
-    # assert part_two(*parse_input(self.example), 500) == 167004
-    # assert part_two(*parse_input(self.example), 1000) == 668697
-    # assert part_two(*parse_input(self.example), 5000) == 16733044
 
 
 def main():
